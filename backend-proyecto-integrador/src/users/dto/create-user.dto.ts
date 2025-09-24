5bfd313c81f6c346182500d07fe81ce99a68aedf
Brayan Alexander Pizarro Bugueño <114868676+brayanpizarro@users.noreply.github.com>
2025-09-24 11:30:09 -0300

    cambios create user update user user entity

--- backend-proyecto-integrador/src/users/dto/create-user.dto.ts
+++ backend-proyecto-integrador/src/users/dto/create-user.dto.ts
@@ -1,27 +1,34 @@
-import { IsString, IsEmail, IsNotEmpty, MinLength, IsOptional, IsBoolean } from 'class-validator';
+import {
+  IsString,
+  IsEmail,
+  IsNotEmpty,
+  MinLength,
+  IsOptional,
+  IsBoolean,
+} from 'class-validator';
 import { Exclude, Expose } from 'class-transformer';
 
 @Exclude()
 export class CreateUserDto {
-    @Expose()
-    @IsString({ message: 'El nombre debe ser una cadena de texto' })
-    @IsNotEmpty({ message: 'El nombre es requerido' })
-    @MinLength(2, { message: 'El nombre debe tener al menos 2 caracteres' })
-    name: string;
+  @Expose()
+  @IsString({ message: 'El nombre debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'El nombre es requerido' })
+  @MinLength(2, { message: 'El nombre debe tener al menos 2 caracteres' })
+  name: string;
 
-    @Expose()
-    @IsEmail({}, { message: 'El correo electrónico no es válido' })
-    @IsNotEmpty({ message: 'El correo electrónico es requerido' })
-    email: string;
+  @Expose()
+  @IsEmail({}, { message: 'El correo electrónico no es válido' })
+  @IsNotEmpty({ message: 'El correo electrónico es requerido' })
+  email: string;
 
-    @Expose()
-    @IsString({ message: 'La contraseña debe ser una cadena de texto' })
-    @IsNotEmpty({ message: 'La contraseña es requerida' })
-    @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
-    password: string;
+  @Expose()
+  @IsString({ message: 'La contraseña debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'La contraseña es requerida' })
+  @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
+  password: string;
 
-    @Expose()
-    @IsOptional()
-    @IsBoolean({ message: 'isActive debe ser un valor booleano' })
-    isActive?: boolean;
+  @Expose()
+  @IsOptional()
+  @IsBoolean({ message: 'isActive debe ser un valor booleano' })
+  isActive?: boolean = true;
 }
