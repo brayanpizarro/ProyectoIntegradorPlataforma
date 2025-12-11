f00258cb316867055e0f52e6c6eae186547650cb
Isaias Gavia <166059945+Resttoga@users.noreply.github.com>
2025-12-10 22:09:41 -0300

    Actualizar authService.ts
    modificacion api url

--- FrontEndProyectoIntegrador/src/services/authService.ts
+++ FrontEndProyectoIntegrador/src/services/authService.ts
@@ -4,7 +4,7 @@
 
 import type { LoginCredentials, AuthResponse, Usuario } from '../types';
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 class AuthService {
   private currentUser: Usuario | null = null;
